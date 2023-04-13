6b6a7b7a4a117dba7391c81356c13ba4d7caf749
Edwin Lock <edwinlock@users.noreply.github.com>
2023-04-13 23:51:56 +0100

    finished iterator

--- marginalreturns/iterator.jl
+++ marginalreturns/iterator.jl
@@ -1,19 +1,15 @@
-function _iterate(state::Vector{Int}, max_vals)
-    state = copy(state)
-    state[end] += 1
-    i = length(state)
-    while i > 1 && state[i] > max_vals[i]  # carry over!
-        state[i-1] += 1
-        i -= 1
-    end
-    state[1] > max_val[1] && return nothing
-    state[i+1:end] .= state[i]
-    return state
-end
+"""
+General notes: The aim is to implement a Julia iterator according to the following documentation.
+https://docs.julialang.org/en/v1/manual/interfaces/
+
+TODO:
+* Implement a length function. We should be able to express the length as a formula, but I haven't thought about it yet... This is important because then the ProgressBar works again.
+* Implement any other optional methods?
+"""
 
 struct LexiIter
-    inner
-    rep::Int
+    inner  # the 'inner' iterator that is repeated
+    rep::Int  # the number of repetitions
 end
 
 import Base.iterate
@@ -23,30 +19,26 @@ function iterate(iter::LexiIter)
     isnothing(res) && return nothing
     inner_item, inner_state = res
     item = ntuple(_ -> inner_item, iter.rep)
-    state = ntuple(_ -> 1, iter.rep)
+    state = ntuple(_ -> (inner_item, inner_state), iter.rep)
     return item, state
 end
 
 function iterate(iter::LexiIter, state::Tuple)
     next_state = _iterate_state(iter, state)
     isnothing(next_state) && return nothing
-    next_item = map(s -> first(iterate(iter.inner, s-1)), next_state)
+    next_item = map(s -> first(s), next_state)
     return next_item, next_state
 end
 
 function _iterate_state(iter::LexiIter, state::Tuple)
-    inner_states = [s for s in state]
-    res = iterate(iter.inner, inner_states[end])
+    next_state = [s for s in state]
     i = iter.rep
-    while i > 1 && isnothing(res)  # find smallest index which doesn't return nothing on advancing
+    res = iterate(iter.inner, last(state[i]))
+    while i > 1 && isnothing(res)  # find largest index which doesn't return nothing on advancing
         i -= 1
-        res = iterate(iter.inner, inner_states[i])
+        res = iterate(iter.inner, last(state[i]))
     end
     i==1 && isnothing(res) && return nothing
-    _, s = res
-    while i <= iter.rep
-        inner_states[i] = s
-        i += 1
-    end
-    return tuple(inner_states...)
+    for k in i:iter.rep; next_state[k] = res; end
+    return tuple(next_state...)
 end
